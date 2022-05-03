15ed513d9e64bae8c74cb13007d02a3ee803d7f3
James Woodworth <jameswoodworth2022@u.northwestern.edu>
2022-05-03 14:42:59 -0500

    added a few comments

--- jamie/my-app/src/components/Login.jsx
+++ jamie/my-app/src/components/Login.jsx
@@ -32,6 +32,7 @@ const Login = () => {
                 <form onSubmit={handleSubmit}>
                     <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} className='input' placeholder="Username" required />
                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+                    {/*maybe the line above isn't converting to right type*/}
                     <div align = 'center'>
                         <button type='submit' className='button'>
                             <span>Start chatting</span>
