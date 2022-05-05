929188e47808c33729731ead55060c6b811d9eb9
Andrew Chung <andrewchung2022@u.northwestern.edu>
2022-05-05 13:28:40 -0500

    complete chat interface

--- jamie/my-app/src/App.js
+++ jamie/my-app/src/App.js
@@ -5,13 +5,6 @@ import Feed from "./components/Feed";
 
 const App = () => {
   return (
-    // <ChatEngine
-    //   height='100vh'
-    //   projectID = 'c4d3b966-c2e6-4806-8ede-3039893da1e3'
-    //   userName= {localStorage.getItem('username')} //'Kris'
-    //   userSecret= {localStorage.getItem('password')} //'338'
-    //   renderChatFeed = {(chatAppProps) => <ChatFeed { ... chatAppProps}/> }
-    // />
     <Fragment>
       <div>Dr Know It All</div>
       <Feed />
--- jamie/my-app/src/components/Feed.jsx
+++ jamie/my-app/src/components/Feed.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-const Feed = (props) => {
+const Feed = () => {
   const users = ["donald", "elon", "joe", "ts", "buddha"];
   const [prompt, setPrompt] = useState("Tell me a funny story.");
   const [messages, setMessages] = useState([]);
@@ -11,30 +11,37 @@ const Feed = (props) => {
     setIndex(newIndex);
   };
 
-  useEffect(() => {
-    (async () => {
-      const res = await fetch("http://127.0.0.1:5000/generate", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-        mode: "cors",
-        body: JSON.stringify({
-          prompt,
-          person: users[index],
-        }),
-      });
-      const data = await res.json();
-      setPrompt(data.generated_text);
-    })();
-  }, []);
+  const addWord = async () => {
+    const res = await fetch("http://127.0.0.1:5000/generate", {
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify({
+        prompt,
+        person: users[index],
+      }),
+    });
+
+    const data = await res.json();
+    const text = data.generated_text;
+    const words = text.split(" ");
+    const newWord = words[words.length - 1];
+
+    setPrompt(text);
+    messages.push({ person: users[index], word: newWord });
+    setMessages(messages);
+    changeIndex();
+  };
 
   return (
     <div>
-      {prompt}
-      {/* {messages.map((message) => (
-        <Message message={message} />
-      ))} */}
+      <div>{prompt}</div>
+      <button onClick={addWord}>Continue Story</button>
+      {messages.map(({ person, word }) => (
+        <div>
+          {person}: {word}
+        </div>
+      ))}
     </div>
   );
 };
