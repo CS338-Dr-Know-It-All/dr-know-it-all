9d2ee91ddb639404abccdd01d71dfa37c0aa552e
James Woodworth <jameswoodworth2022@u.northwestern.edu>
2022-05-24 11:15:16 -0500

    updated UI with profile pictures

--- jamie/my-app/src/components/Message.jsx
+++ jamie/my-app/src/components/Message.jsx
@@ -1,18 +1,35 @@
 import { Card } from "react-bootstrap";
 
 const Message = (props) => {
+
+    var headshotDict = {
+        'donald' : 'https://www.politico.com/interactives/uploads/image-service/2019/09/trump.png' ,
+        'joe' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWBLa7Fu80Vpqts9GQBEwRybgV9G_YfnDvdw&usqp=CAU' ,
+        'elon' : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFNagohyR6WUNJ0tw7oRFxPDeXztYsQISr5g&usqp=CAU',
+        'shakespear': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg3PLHmsdbAlAeERzQCKPsxqfS8tCnCuHsgA&usqp=CAU',
+        'buddha': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5ClvHKjBY0r97MLA52zZcH6HdgSS112uNww&usqp=CAU' 
+    };
+
+
     return (
         <div className="message-row">
-            <Card
-                className="message"
-                style={{
-                    float: "left",
-                    backgroundColor: "#CABCDC",
-                    marginLeft: "48px",
-                }}
-            >
-                {props.person} : {props.word}
-            </Card>
+            <div className="message-avatar">
+                <img src= {headshotDict[props.person]} 
+                alt='character headshot' 
+                width= '44'
+                heigh= '44'/>
+            </div>
+
+            <div className="message-block">
+                <Card className="message"
+                    style={{
+                        float: "left",
+                        backgroundColor: "#CABCDC",
+                        marginLeft: "18px"
+                    }} >   
+                    {props.word}
+                </Card>
+            </div>
         </div>
     );
 };
